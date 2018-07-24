6e63cbf5ba13b1e51577786b02e2717e9fb46e4a
Meigo Lien <meigo@MeigodeMacBook-Pro.local>
2018-07-24 22:56:35 +0800

    test

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Meigo on 2018/7/24.
+//  Copyright © 2018年 Meigo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
